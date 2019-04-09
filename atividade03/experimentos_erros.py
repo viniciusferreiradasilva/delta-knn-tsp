f08b480029c9fd19acc43c0718c0efbedee06d35
Vinícius Ferreira <viniciusferreira97@gmail.com>
2019-04-09 19:36:40 -0300

    inserindo arima

--- atividade03/experimentos_erros.py
+++ atividade03/experimentos_erros.py
@@ -1,12 +1,12 @@
-# Implementação da atividade 02 de Aplicações de Algoritmos de Aprendizado de Máquina.
+# Implementação da atividade 03 de Aplicações de Algoritmos de Aprendizado de Máquina.
 from statsmodels.tsa.ar_model import AR
-from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+import math
 
 # Instancia o parser.
 parser = argparse.ArgumentParser(description='Ferramenta de regressão.', formatter_class=argparse.RawTextHelpFormatter)
@@ -40,16 +40,21 @@ parser.add_argument('--window', type=int, default=20,
 parser.add_argument('--steps', type=int, default=5,
                     help='um valor inteiro que representa o número de passos futuros que serão previstos.')
 
+# Argumento para o modelo de regressão.
+parser.add_argument('--alg', type=int, default=0,
+                    help='Um valor inteiro que representa qual algoritmo deve ser usado. 0 - AR\n1 - ARIMA\n2 - SARIMA.')
+
 args = parser.parse_args()
 
 # Carrega o arquivo .csv em um dataframe do pandas.
 df_full = pd.read_csv(args.input).dropna()
+
 # Seleciona apenas as linhas que contém o nome da ação passado como parâmetro.
 df = df_full.loc[df_full['Name'] == args.stock_name]
 
-print('dataframe com ', len(df), ' linhas carregadas.')
 # Seta onde começa a predição.
 from_index = args.from_index
+
 # Seta até onde vai a predição.
 if(args.to_index):
     to_index = args.to_index
@@ -57,34 +62,69 @@ else:
     to_index = len(df)
 # Seta o nome do campo que será utilizado na regressão.
 field = args.field
+
 # Seta o tamanho das janelas que será utilizada na regressão.
 window_size = args.window
+
 # Seta o tamanho do passo que será predito.
 step_size = args.steps
+
 # Recupera a série do dataframe de acordo com o nome fornecido como argumento.
 series = df[from_index:to_index][field]
 
 # Arrays que armazenam os valores reais.
 y = [None] * (len(series) - window_size)
+
 # Arrays que armazenam os valores preditos.
 predicted = [None] * (len(series) - window_size)
+
+# Recupera o algoritmo que vai ser utilizado na regressão.
+algorithm = [AR, ARIMA][args.alg]
+
 
 for i in range(0, (len(series) - window_size), step_size):
     # Recupera uma fatia da série temporal de acordo com o tamanho da janela.
     train_series = series[i:(i + window_size)].values
+
     # Cria um modelo de autoregressão.
-    model = AR(train_series).fit()
+    # model = algorithm(train_series)
+    # model = algorithm(train_series, order=(1, 0, 0))
+    model = ARIMA(train_series, order=(1, 0, 0))
+    # Ajusta o modelo de acordo com a faixa da série.
+    model = model.fit(disp=0)
+
     # Prediz os step_size passos na série temporal.
     prediction = model.predict(start=len(train_series), end=(len(train_series) + step_size - 1), dynamic=False)
     # Armazena o valor predito.
     predicted[i:(i + step_size)] = prediction
     # print("lag:", model.k_ar)
 
-y = series[window_size:]
+y = series[window_size:].values
 predicted = predicted[:len(y)]
 
+# Calcula o erro quadrático médio.
+mean_square_error = mean_squared_error(y, predicted)
+
+# Calcula a lista contendo os erros absolutos.
+absolute_errors = list(map(lambda x, y: math.fabs((x - y)), y, predicted))
+
+# Calcula o TU.
+predictor = 0
+naive_predictor = 0
+for i in range(1, len(y)):
+    predictor += (y[i] - predicted[i]) * (y[i] - predicted[i])
+    naive_predictor += (y[i] - y[i - 1]) * (y[i] - y[i - 1])
+tu = predictor / naive_predictor
+
+# Calcula o POCID.
+pocid = 0
+for i in range(1, len(y)):
+    if((predicted[i] - predicted[i - 1]) * (y[i] - y[i - 1])) > 0:
+        pocid += 1
+pocid /= len(y)
+
 # Plota o gráfico diferenciando os valores reais e os valores preditos.
-# plt.subplot(2, 1, 1)
+plt.subplot(2, 1, 1)
 plt.plot(range(len(predicted)), predicted, label='predito')
 plt.plot(range(len(y)), y, label='y')
 plt.legend()
@@ -92,17 +132,18 @@ plt.title("Resultados de AutoRegressão para " + args.stock_name)
 plt.xlabel("day")
 plt.ylabel("open")
 
-# # Calcula o erro quadrático médio.
-mean_square_error = mean_squared_error(y.values, predicted)
-print(mean_square_error)
-# # Plota o gráfico do erro quadratico para cada ponto.
-# plt.subplot(2, 1, 2)
-# errors = list(map(lambda x, y: (x - y) * (x - y), y, predicted))
-# plt.plot(range(len(errors)), errors, label='erro')
-# plt.legend()
-# plt.title("Erros na AutoRegressão para " + args.stock_name + " (erro médio = " + '{:.3f}'.format(mean_square_error) + ")")
-# plt.xlabel("day")
-# plt.ylabel("erro")
-# plt.subplots_adjust(hspace=.5)
-# plt.show()
-plt.savefig('output/' + args.stock_name+'_main.eps', format='eps')
+# Métricas de erro.
+print('mean_square_error:', mean_square_error)
+print('TU:', tu)
+print('pocid:', pocid)
+
+# Plota o gráfico do erro quadratico para cada ponto.
+plt.subplot(2, 1, 2)
+plt.plot(range(len(absolute_errors)), absolute_errors, label='erro')
+plt.legend()
+plt.title("Erros na AutoRegressão para " + args.stock_name + " (erro médio = " + '{:.3f}'.format(mean_square_error) + ")")
+plt.xlabel("day")
+plt.ylabel("erro")
+plt.subplots_adjust(hspace=.5)
+plt.show()
+# plt.savefig('output/' + args.stock_name+'_main.eps', format='eps')
